bf9aceec730f05bad7243d1f95b5fc4e0c97baf0
Nathanael Kouassi <nathanaelkouassi55@gmail.com>
2026-05-26 02:13:06 +0000

    Fix vite config - remove manualChunks

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    port: 3000,
-    fs: { allow: ['.'] }
-  },
-  build: {
-    target: 'es2022',
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['svelte', '@sveltejs/kit']
-        }
-      }
-    }
-  }
+  plugins: [sveltekit()]
 });
